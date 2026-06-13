71499356bd97698cf115997113cdd445744d0440
Anasimandro9 <anas.lakti123@gmail.com>
2026-06-13 11:24:11 +0200

    fix: explicit types for item position to fix compose inferrence

--- app/src/main/java/org/oxycblt/auxio/playback/queue/QueueTab.kt
+++ app/src/main/java/org/oxycblt/auxio/playback/queue/QueueTab.kt
@@ -61,7 +61,9 @@ fun QueueTab(queueModel: QueueViewModel, playbackModel: PlaybackViewModel) {
 
     val reorderState =
         rememberReorderableLazyListState(
-            onMove = { from, to -> queueModel.moveQueueDataItems(from.index, to.index) }
+            onMove = { from: org.burnoutcrew.reorderable.ItemPosition, to: org.burnoutcrew.reorderable.ItemPosition -> 
+                queueModel.moveQueueDataItems(from.index, to.index) 
+            }
         )
     val listState = reorderState.listState
 
